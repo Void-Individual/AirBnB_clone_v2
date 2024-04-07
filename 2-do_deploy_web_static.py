757bf36caa789e33c48a6007c022651e5e58e969
Void-Individual <otayomitv@gmail.com>
2024-04-07 23:59:16 +0100

    time to test my do deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Fabric script to distribute an archive to a web server"""
+
+from fabric.api import run, env, put
+from os.path import exists
+
+
+env.hosts = ['52.91.151.166', '35.174.211.254']
+
+def do_deploy(archive_path):
+    """A function to distribute an archive to a remote server"""
+
+    if not exists(archive_path):
+        return False
+
+    try:
+        # Upload the archive to tmp
+        put(archive_path, '/tmp/')
+
+        # Extract the archive
+        # Retieve the name of the file in the archive directory
+        filename = archive_path.split('/')[-1]
+        # Retrieve the name of the file without its extension
+        foldername = filename.split('.')[0]
+        # Create the location to extract it to if it doesn't exist
+        run('mkdir -p /data/web_static/releases/{}'.format(foldername))
+        # Extract the compressed file to the desired location
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, foldername))
+        # Delete the transferred archive
+        run('rm -rf /tmp/{}'.format(filename))
+        # Delete the current symlink
+        run('rm /data/web_static/current')
+        # Create a new one to the desired location
+        run(f'ln -s /data/web_static/releases/{foldername} /data/web_static/current')
+
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
