59ee4f1964ccefaa835738f0e951f47b48bde304
Void-Individual <otayomitv@gmail.com>
2024-04-21 11:35:12 +0100

    fixed doc errors on tasks up to 6

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Let the python app display an additional function
+"""Let the python app display an additional function
+"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -10,15 +11,18 @@ def start():
     # Function to display the index page
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     # Function to display the hbnb page
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def C_text(text):
     # Function to display a page after /c
-    return  f"C {text}"
+    return f"C {text}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Let the python app display an additional function
+"""Let the python app display an additional function
+"""
 
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -10,34 +11,41 @@ def start():
     # Function to display the index page
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     # Function to display the hbnb page
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def C_text(text):
     # Function to display a page after /c
-    return  f"C {text}"
+    return f"C {text}"
+
 
 @app.route('/python/', strict_slashes=False)
 def python_default():
     return "Python is cool"
 
+
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     # Function to display a page after /python
-        return f"Python {text}"
+    return f"Python {text}"
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
     # Function to display a page after a /number
     return f"{n} is a number"
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     # Function to display an html page
     return render_template('5-number.html', n=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
