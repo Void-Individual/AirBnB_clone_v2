74c3b4b7e9b712cfa29cdfa97961cf60975d01a0
Void-Individual <otayomitv@gmail.com>
2024-04-21 11:29:53 +0100

    extra space and changed doc format

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# A script that starts a flask app with new routes
+"""A script that starts a flask app with new routes
+"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -10,10 +11,12 @@ def start():
     # Function to display the index page
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     # Function to display the hbnb page
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
