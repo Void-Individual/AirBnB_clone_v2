c003dad0ae4d2b0d3b491e8a08eff3a0185e3909
Void-Individual <otayomitv@gmail.com>
2024-04-21 14:08:45 +0100

    Lets see if it checks

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
-# Script to start flask and run 
+"""Script to start flask and run states
+"""
+
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_the_db(exception=None):
+    # Function to clear previous session
+    storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    # Function to display html page full of states
+    from models.state import State
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
