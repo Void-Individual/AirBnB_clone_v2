ab3c1d9c9edd392ce173a7904e214c64c1cf9840
Void-Individual <otayomitv@gmail.com>
2024-04-21 18:17:02 +0100

    Made my file executable

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,12 +12,14 @@ def close_the_db(exception=None):
     # Function to clear previous session
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     # Function to display html page full of states
     from models.state import State
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
