937a1a6b966ec464e2e8811e04dee1d43160dc7c
Void-Individual <otayomitv@gmail.com>
2024-05-14 18:33:33 +0000

    edited hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def start():
     # Function for the index call
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000)
